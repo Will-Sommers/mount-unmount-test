256a26db6715d681735ef867b68677c2bfc1268a
Will Sommers <will.sommers@gmail.com>
2014-06-22 11:33:27 -0400

    Looking at how mount and unmount works

--- src/mount_unmount_test/core.cljs
+++ src/mount_unmount_test/core.cljs
@@ -6,8 +6,41 @@
 
 (def app-state (atom {:text "Hello world!"}))
 
+(defn test-component [data owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (println "I have mounted"))
+
+    om/IRender
+    (render [_]
+      (dom/div nil (:yar data)))
+
+    om/IWillUnmount
+    (will-unmount [_]
+      (println "I have unmounted"))))
+
+
+
+(defn app-component [data owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+       (if (:child-component data)
+         (om/build test-component (:child-component data))
+         (dom/strong nil "Not building child component"))))))
+
+(defn change-state-atom [app-state]
+  (.setInterval js/window
+                #(if (get @app-state :child-component)
+                   (reset! app-state {:nope "nope"})
+                   (reset! app-state {:child-component {:yar "Hello, child component here."}}))
+                1000))
+
+(change-state-atom app-state)
+
 (om/root
-  (fn [app owner]
-    (dom/h1 nil (:text app)))
+  app-component
   app-state
   {:target (. js/document (getElementById "app"))})
